6da0c8a0e12b7aa91ffb4d06d5ebecf318f234cc
faszjoska <kalocsaroland.exe@gmail.com>
2025-09-10 12:26:12 +0200

    szam lista gen

---
+++ elso.py
@@ -0,0 +1,12 @@
+import random
+
+
+# lista létrehozasa
+lista100=[]
+
+# lista feltoltese random 100 db ketjegyu szamokkal
+for i in range(100):
+    szam=random.randint(10,99)
+    lista100.append(szam)
+print(len(lista100))
+#muxik
