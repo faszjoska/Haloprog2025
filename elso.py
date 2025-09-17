fbc7258ecd570c6c9cd8f187716195354771983e
faszjoska <kalocsaroland.exe@gmail.com>
2025-09-17 11:25:51 +0200

    befejezett_egyszamjatek

--- elso.py
+++ elso.py
@@ -13,15 +13,14 @@ print(len(lista100))
 #muxik
 
 # egyszam jatek
-jatek_szam=0
+jatek_szam=1
 nem_talaltdb=0
 
 kitalallado_szam=lista100[random.randint(0,len(lista100))] 
 
 jatszol=True
 while jatszol:
-    jatek_szam+=1
-    tipp_sz=input("Adj egy szamot:").strip()
+    tipp_sz=input("\nAdj egy szamot:").strip()
     
     if tipp_sz.isdecimal():
         tipp=int(tipp_sz)
@@ -29,21 +28,30 @@ while jatszol:
         print("Egész számot kell megadni")
         continue
     while tipp != kitalallado_szam:
+        nem_talaltdb+=1
+
+        if tipp==123:
+            pass
         if tipp< kitalallado_szam:
-            print("Nagyobb a szám")
+            print("Nagyobb a szám>")
+        elif tipp> kitalallado_szam:
+            print("Kissebb a szám<")
         else:
-            print("Kissebb a szám")
-        tipp_sz=input("Adj egy szamot:\n(ha ki akarsz lépni EXIT)").strip()
+            pass 
+        tipp_sz=input("\nAdj egy szamot (ha ki akarsz lépni EXIT):").strip()
         if tipp_sz.isdecimal():
             tipp=int(tipp_sz)
         elif tipp_sz=="EXIT":
             exit()
         else:
             print("Egész számot kell megadni")
+            tipp=123
             continue
     print('jo a tipp')
 
     folytatas=input("Akarasz még játszani? [I/N]:")
     if folytatas == "N":
         jatszol=False
-
+        print(f"{jatek_szam}db alkalommal játszott,{nem_talaltdb}db ennyiszer tippelt mellé")
+    elif folytatas == "I":
+        jatek_szam+=1
